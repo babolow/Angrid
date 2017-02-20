63715890163506445cdb2b6ce177c6b32582aa85
Babolow <vigerthomas76@gmail.com>
2017-02-20 10:23:27 +0100

    first commit

---
+++ src/Angrid_Api/Repository/UtilisateurRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angrid_Api.Repository
+{
+    public class UtilisateurRepository
+    {
+    }
+}
---
+++ src/Angrid_Web/wwwroot/app.js
@@ -0,0 +1,7 @@
+﻿angular.module('demo', [])
+.controller('InfoAPIController', function ($scope, $http) {
+    $http.get('http://localhost:57347/api/values/4').
+        then(function (response) {
+            $scope.titi = response.data;
+        });
+});
